530e550aaf2ca62a52b64e4f39db74b962767984
Debast Yves <debyves@hotmail.com>
2016-09-29 17:08:23 +0200

    cocommit

---
+++ Labo3BD/UnitTestProject1/DbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Entity;
+using Labo3BD;
+
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    public class DbInitializer:DropCreateDatabaseAlways<CompanyContext>
+    {
+        protected override void Seed(CompanyContext context)
+        {
+            Customer customer = new Customer()
+            {
+                Name = "Albert Dupont",
+                AddressLine1 = "Rue des cerisiers, 16",
+                City = "Arlon",
+                Country = "Belgique",
+                EMail = "info@me.com",
+                Id = 3,
+                Remark = "bfzkfzkfz",
+                PostCode = "6700"
+            };
+            context.Customers.Add(customer);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Labo3BD/UnitTestProject1/UnitTestProject1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Labo3BD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestProject1
+    {
+        public CompanyContext GetContext()
+        {
+
+            return new CompanyContext();
+        }
+
+        [TestInitialize]
+        public void setup()
+        {
+            Database.SetInitializer(new DbInitializer());
+            using (CompanyContext context = GetContext())
+            {
+                context.Database.Initialize(true);
+            }
+        }
+
+        [TestMethod]
+        public void CanGetCustomers()
+        {
+            using (var context = GetContext())
+            {
+                Assert.AreEqual(1,context.Customers.ToList().Count);
+            }
+        }
+    }
+}
